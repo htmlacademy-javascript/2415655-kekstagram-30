b69c761c12683bee8a6a14d0c6ab5119fad33b7f
vanzo16-github <141827057+vanzo16-github@users.noreply.github.com>
2023-11-06 13:19:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {MAX_PHOTO, getPhotosDesc} from './data.js';
-import {renderPhotos} from './photos.js';
+import {renderPhotos} from './photos-error.js';
+import './photos-error.js';
 
 const photoArray = getPhotosDesc(MAX_PHOTO);
 renderPhotos(photoArray);
